f9fbcba8d7645540980254a703de96d6dc1db2ca
dedeledindon <wwillmat@gmail.com>
2017-08-18 14:25:56 +0200

    oi

--- src/main/java/com/sopra/resa/app/ResaApp.java
+++ src/main/java/com/sopra/resa/app/ResaApp.java
@@ -4,17 +4,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.sopra.resa.service.ServiceClient;
 
+//8
 public class ResaApp {
 
 	public static void main(String[] args) {
 		System.out.println("resaApp");
 		try {
-			ClassPathXmlApplicationContext context =
-					new ClassPathXmlApplicationContext("configSpring.xml");
-			
-			ServiceClient serviceClient = (ServiceClient)
-					context.getBean("serviceClientImpl"/*id*/);
-			
+			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("configSpring.xml");
+
+			ServiceClient serviceClient = (ServiceClient) context.getBean("serviceClientImpl"/* id */);
+
 			System.out.println(serviceClient.rechercherClient(1L));
 			context.close();
 		} catch (Exception e) {
